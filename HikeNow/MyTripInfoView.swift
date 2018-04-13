38d60fe908da711b79c77c4aafac1b0140efa776
Alex Bumpers <apbumpers@gmail.com>
2018-04-13 11:17:25 -0700

    Add labels to new view, add view controller

--- HikeNow/MyTripInfoView.swift
+++ HikeNow/MyTripInfoView.swift
@@ -6,4 +6,10 @@
 //  Copyright © 2018 Alex Bumpers. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MyTripInfoView: UIViewController {
+    // TODO: Move stored and encoded data to this view and edit text accordingly
+    // TODO: Implement NPS alerts from NPS API
+    // TODO: Implement weather information from Weather Underground (?) API
+}
